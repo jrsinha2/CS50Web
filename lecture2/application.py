1180420e930ca1dfb7b9adeac701cb42389650cf
Aditya Sinha <aditya.sinha99@gmail.com>
2019-05-09 00:45:16 +0530

    rendering templates

--- lecture2/application.py
+++ lecture2/application.py
@@ -3,5 +3,6 @@ app = Flask(__name__)
 #flask always look to templates directory for rendering templates...
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headLine = "Hello World!"
+    return render_template("index.html",headline=headLine)
 
